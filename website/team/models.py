bea6df10de5bfe6a6a4cf61d8d2fe7637e849cf0
Abha Jha <jhaabha08@gmail.com>
2020-08-07 22:25:45 +0530

    teams done

--- website/team/models.py
+++ website/team/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Members(models.Model):
+    name=models.CharField(max_length=100,default=None)
+    position= models.CharField(blank=True,max_length=100,default=None)
+    image=models.ImageField(blank=True, upload_to='memberImages/',default=None)
+    year=models.IntegerField(default=None)
+    facebook=models.URLField(blank=True,max_length=200,default=None)
+    linkedin=models.URLField(blank=True,max_length=200,default=None)
+    email=models.EmailField(blank=True,max_length=254,default=None)
+
+    def __str__(self):
+        return self.name
--- website/team/views.py
+++ website/team/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
+from .models import Members
 
-def team(requests):
-    return render(requests,'team/team_page.html')
+# Create your views here.
+
+def team(request):
+    members=Members.objects.all()
+    context={}
+    # names=[member.name for member in members]
+    # year=[member.year for member in members]
+    # images=[member.image for member in members]
+    positions=[member.position for member in Members.objects.all()]
+    
+    third_year= Members.objects.filter(year=3).order_by('name')
+    second_year=Members.objects.filter(year=2).order_by('name')
+
+    position_holders=[]
+
+    for member in Members.objects.filter(position='President'):
+        position_holders.append(member)
+    for member in Members.objects.filter(position='General Secretary'):
+        position_holders.append(member)
+    for member in Members.objects.filter(position='Treasurer'):
+        position_holders.append(member)
+    for member in Members.objects.filter(position='Vice President'):
+        position_holders.append(member)
+    for member in Members.objects.filter(position='Convenor'):
+        position_holders.append(member)
+    for member in Members.objects.filter(year=4).exclude(position='President').exclude(position='Vice President').exclude(position='General Secretary').exclude(position='Convenor').exclude(position='Treasurer'):
+        position_holders.append(member)
+
+    # print(position_holders)
+
+    context['final']=position_holders
+    context['third']=[third for third in third_year]
+    context['second']=[second for second in second_year]
+    
+    return render(request,'team/team_page.html',context)
