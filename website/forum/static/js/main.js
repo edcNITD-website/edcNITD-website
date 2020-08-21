ba8b38b0763d242e05bbc600c81d639073d3af66
subhajoy <scrapsubhajoy@gmail.com>
2020-08-21 12:32:38 +0530

    Adding Slider

--- website/forum/static/js/main.js
+++ website/forum/static/js/main.js
@@ -17,6 +17,25 @@
     }
   });
 
+  //swiper
+  var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+	
+
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
