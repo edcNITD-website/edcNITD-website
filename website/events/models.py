7abbe6e8aa4a3da7c8a507ee2eb0db68085f6c25
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2020-09-14 22:32:14 +0530

    Now showing events according to Date and Month

--- website/events/models.py
+++ website/events/models.py
@@ -1,17 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class UpcomingEvent(models.Model):
+
+class Event(models.Model):
     event_name=models.CharField(max_length=15)
-    about_event=models.TextField()
-    image1=models.ImageField(upload_to=None, default="")
+    about_event=models.TextField(max_length=500)
+    image1=models.ImageField(upload_to=None, default="",blank=True,null=False)
+    current_month=models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(12)])
+    current_day=models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(31)])
+
 
     def __str__(self):
         return self.event_name
-
-class OtherEvent(models.Model):
-    eventname=models.CharField(max_length=15)
-    aboutevent=models.TextField()
-    img1=models.ImageField(upload_to=None, default="")
-
-    def __str__(self):
-        return self.eventname
