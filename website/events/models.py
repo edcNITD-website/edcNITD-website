ac1e6082c22d4ced16a24a68913c2c76622de2bc
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2020-09-18 21:11:08 +0530

    Added registration button

--- website/events/models.py
+++ website/events/models.py
@@ -9,6 +9,7 @@ class Event(models.Model):
     image=models.ImageField(upload_to=None, default="",blank=True,null=False)
     start_date=models.DateTimeField(auto_now=False,auto_now_add=False,blank=True,null=True)
     end_date=models.DateTimeField(auto_now=False,auto_now_add=False,blank=True,null=True)
+    registration_link=models.URLField(blank=True,null=True)
     
     def __str__(self):
         return self.event_name
