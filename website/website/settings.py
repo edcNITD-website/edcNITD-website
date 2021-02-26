7042b795f83f4c85271d3dd9be2e952109189416
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-02-26 14:01:18 +0530

    CSS error removed and code updated

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_iin05bk$ipxvl=_-gi5-4n44ns(+(*kw-ku^oqts!x@lvl_m8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
