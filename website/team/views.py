4f472351ca94e16cafc039ffca44296610de3a9b
Ritabrata Das <76585827+RitabrataDas343@users.noreply.github.com>
2021-06-12 17:34:49 +0530

    updated views.py

--- website/team/views.py
+++ website/team/views.py
@@ -36,11 +36,11 @@ def team(request):
     context['third']=[third for third in third_year]
     context['second']=[second for second in second_year]
     alumni_dic = dict()
-    alumni_list = Members.objects.filter(passing_out_date__lt=timezone.now().date())
+    alumni_list = Members.objects.filter(passing_out_date__lt=timezone.now().date()).order_by('name')
     print(alumni_list)
     starting_year=timezone.now().date().year
     while starting_year>1990:
-        current_year_alumni = alumni_list.filter(passing_out_date__year=starting_year)
+        current_year_alumni = alumni_list.filter(passing_out_date__year=starting_year).order_by('name')
         print(current_year_alumni)
         if current_year_alumni.count() == 0:
             starting_year-=1
