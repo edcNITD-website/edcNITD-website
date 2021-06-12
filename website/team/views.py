90a3c2662029296908af0de8705c11961cd71d61
Ritabrata Das <76585827+RitabrataDas343@users.noreply.github.com>
2021-06-12 16:56:06 +0530

    modified views.py for alumni section

--- website/team/views.py
+++ website/team/views.py
@@ -32,15 +32,15 @@ def team(request):
 
     # print(position_holders)
 
-     context['final']=position_holders
+    context['final']=position_holders
     context['third']=[third for third in third_year]
     context['second']=[second for second in second_year]
     alumni_dic = dict()
-    alumni_list = Members.objects.filter(passing_out_date__lt=timezone.now().date()).order_by('name')
+    alumni_list = Members.objects.filter(passing_out_date__lt=timezone.now().date())
     print(alumni_list)
     starting_year=timezone.now().date().year
     while starting_year>1990:
-        current_year_alumni = alumni_list.filter(passing_out_date__year=starting_year).order_by('name')
+        current_year_alumni = alumni_list.filter(passing_out_date__year=starting_year)
         print(current_year_alumni)
         if current_year_alumni.count() == 0:
             starting_year-=1
@@ -51,13 +51,9 @@ def team(request):
             alumni_dic[starting_year]=current_list 
             starting_year-=1
     context['alumni_dic'] = alumni_dic
-    year_list=[]
+    year_list=[0]
     for year in alumni_dic:
         year_list.append(year)
     max_year = max(year_list)
-    context['max_alumni_year']=max_year
-    print(max_year)
-    # print(alumni_dic)
-    print(context)
+    context['max_alumni_year'] = max_year
     return render(request,'team/team_page.html',context)
-
