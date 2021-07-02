d9914b1472e9181533f2330ae5a1fc95f4f4d7d2
mehul-menon <menonm98@gmail.com>
2021-07-03 00:38:48 +0530

    backend updated

--- website/web_team/admin.py
+++ website/web_team/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Members
+from .models import Contributors
 
 # Register your models here.
 
-admin.site.register(Members)
+admin.site.register(Contributors)
--- website/web_team/views.py
+++ website/web_team/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Members
+from .models import Contributors
 
 # Create your views here.
 
 def web_team(request):
-    return render(request,'web_team/web_team.html')
+    context={
+        'contributors':Contributors.objects.all()
+    }
+    ordering=['date']
+    return render(request,'web_team/web_team.html',context)
+
