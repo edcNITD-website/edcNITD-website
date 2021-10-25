016d3761f9730ea6b226e3a6b9aa9ea1ccc1fb44
satwik-7 <83957256+satwik-7@users.noreply.github.com>
2021-10-26 02:07:35 +0530

    frontend design updated almost ready for completion

--- website/EQuest/views.py
+++ website/EQuest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Post
+from django.db.models import Count
 
 def home_page(request):
     context={
@@ -12,16 +13,21 @@ def post_list(request,category_slug=None):
     category=None
     categories=Category.objects.all()  # <appname>/<model>_<viewtype>.html
     post=Post.objects.all()
+    count=Post.objects.all().count()
     if category_slug:
         category=get_object_or_404(Category,slug=category_slug)
         post=post.filter(category = category)
     return render(request,'EQuest/post_list.html',{'categories':categories,
                                                    'category':category,
                                                    'post': post,
+                                                   'count': count,
                                                   })
 def post_detail(request,id):
     post=get_object_or_404(Post,id=id) 
     category=None
     categories=Category.objects.all()
+    count=Post.objects.all().count()
     return render(request,'EQuest/post_detail.html',{'post':post,
-                                                    'categories':categories,})
+                                                    'categories':categories,
+                                                    'count': count
+                                                    })
