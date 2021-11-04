995dc0b01c8e041fd305edfe0d17928238889764
RitabrataDas343 <dasritabrata5@gmail.com>
2021-11-04 19:01:16 +0530

    js transferred into static

--- website/events/static/js/slider.js
+++ website/events/static/js/slider.js
@@ -1,20 +1,30 @@
-var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabCursor: true,
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
   centeredSlides: true,
   slidesPerView: "auto",
-  centeredSlides: true,
-  initialSlide: 2,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 10,
-    initialSlide: 3,
-    slideShadows: true
+  loop: true,
+  speed: 600,
+  
+  autoplay: {
+  delay: 3000,
+    disableOnInteraction: false,
   },
+  
+  coverflowEffect: {
+  rotate: 0,
+  stretch: 0,
+  depth: 100,
+  modifier: 2,
+  slideShadows: true,
+  },
+  
   pagination: {
-    el: ".swiper-pagination",
-     clickable: true
-  }
-});
+  el: '.swiper-pagination',
+  clickable: true,
+  },
+  
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+  });
