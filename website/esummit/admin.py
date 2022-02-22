73463dc4a20c1e83dd9582ad30d4d719872e1560
Purvi2401 <purvi2401binani@gmail.com>
2022-02-22 13:24:41 +0530

    renamed Slider->Carousel, modified maxlength

--- website/esummit/admin.py
+++ website/esummit/admin.py
@@ -3,7 +3,7 @@ from esummit.models import *
 
 # Register your models here.
 admin.site.register(Year_Detail)
-admin.site.register(Slider)
+admin.site.register(Carousel)
 admin.site.register(Event)
 admin.site.register(Speaker)
-admin.site.register(Glimpses)
+admin.site.register(Glimpse)
--- website/esummit/models.py
+++ website/esummit/models.py
@@ -7,53 +7,54 @@ class Year_Detail(models.Model):
     year_img = models.ImageField(
         blank=True, upload_to='esummitYear/', default=None)
     year_desc = models.TextField(default="")
+    yt_link = models.URLField(max_length=200,default=None)
     
     def __str__(self) -> str:
         return str(self.year)
     
     
-class Slider(models.Model):
+class Carousel(models.Model):
     year = models.ForeignKey(Year_Detail, on_delete=models.CASCADE)
     event_img = models.ImageField(
         blank=True, upload_to='esummitSlider/', default=None)
-    title = models.CharField(max_length=100, default=None)
+    title = models.CharField(max_length=500, default=None)
     subtitle = models.TextField(default="")
-    btn_content = models.CharField(max_length=100, default=None)
-    btn_link = models.CharField(max_length=100, default=None)
+    btn_content = models.CharField(max_length=200, default=None)
+    btn_link = models.CharField(max_length=200, default=None)
     is_hero = models.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return str(self.year.year)
+        return str(self.year.year)+"_"+self.title
     
 
 class Event(models.Model):
     year = models.ForeignKey(Year_Detail, on_delete=models.CASCADE)
     event_img = models.ImageField(
         blank=True, upload_to='esummitEvents/', default=None)
-    event_title = models.CharField(max_length=100, default=None)
+    event_title = models.CharField(max_length=500, default=None)
     event_detail = models.TextField(default="")
     
     def __str__(self) -> str:
-        return str(self.year.year)
+        return str(self.year.year)+"_"+self.event_title
     
     
 class Speaker(models.Model):
     year = models.ForeignKey(Year_Detail, on_delete=models.CASCADE)
     speaker_img = models.ImageField(
         blank=True, upload_to='esummitSpeakers/', default=None)
-    speaker_title = models.CharField(max_length=100, default=None)
-    speaker_subtitle = models.CharField(max_length=300, default=None)
+    speaker_title = models.CharField(max_length=500, default=None)
+    speaker_subtitle = models.TextField(default="")
     speaker_desc = models.TextField(default="")
     
     def __str__(self) -> str:
-        return str(self.year.year)
+        return str(self.year.year)+"_"+self.speaker_title
     
     
-class Glimpses(models.Model):
+class Glimpse(models.Model):
     year = models.ForeignKey(Year_Detail, on_delete=models.CASCADE)
     glimpse_img = models.ImageField(
         blank=True, upload_to='esummitGlimpses/', default=None)
     glimpse_desc = models.CharField(max_length=100, default=None)
     
     def __str__(self) -> str:
-        return str(self.year.year)
+        return str(self.year.year)+"_"+self.glimpse_desc
